70a8a4e7614e59a62ba58e7ee603665c14545ad2
noname <156155219+zxcloli666@users.noreply.github.com>
2026-03-04 14:56:47 +0500

    refactor: удалены файлы для подготовки к мерджу

--- frontend/preloads/loader.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('loaderAPI', {
-  onProgress: (callback) => {
-    ipcRenderer.on('loader:progress', (event, data) => callback(data));
-  },
-});
--- frontend/scripts/settings-launcher.js
+++
@@ -1,9 +0,0 @@
-// Settings window launcher
-
-function openSettingsModal() {
-  if (typeof ipcRenderer !== 'undefined') {
-    ipcRenderer.send('settings:open');
-  } else {
-    console.error('ipcRenderer not available');
-  }
-}
