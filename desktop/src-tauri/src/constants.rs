5918a1e3b4fe970401369c03efebc1511c0fa65b
noname <156155219+zxcloli666@users.noreply.github.com>
2026-03-17 17:53:32 +0500

    chore(desktop): add env instead constants to prevent bruh moments

--- desktop/src-tauri/src/constants.rs
+++ desktop/src-tauri/src/constants.rs
@@ -1,6 +1,6 @@
 pub const DISCORD_CLIENT_ID: &str = "1431978756687265872";
 
-pub const PROXY_URL: &str = "https://proxy.soundcloud.su";
+pub const PROXY_URL: &str = if let Some(url) = option_env!("PROXY_URL") { url } else { "https://proxy.soundcloud.su" };
 pub const DOMAIN_WHITELIST: &[&str] = &[
     "localhost",
     "127.0.0.1",
--- desktop/src/lib/constants.ts
+++ desktop/src/lib/constants.ts
@@ -1,5 +1,4 @@
-export const API_BASE = 'https://api.soundcloud.su';
-// export const API_BASE = 'http://localhost:3000';
+export const API_BASE = import.meta.env.VITE_API_BASE || 'https://api.soundcloud.su';
 
 export const GITHUB_OWNER = 'zxcloli666';
 export const GITHUB_REPO = 'SoundCloud-Desktop';
